c223b66316342f99453b1a889af0cc1e8c0ecd6b
Victor Hugo <souzavhi@gmail.com>
2017-10-04 18:43:25 -0300

    array

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -6,6 +6,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -13,5 +14,22 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        String [] words = new String[11];
+        words[0] = "zero";
+        words[1] = "one";
+        words[2] = "two";
+        words[3] = "three";
+        words[4] = "four";
+        words[5] = "five";
+        words[6] = "six";
+        words[7] = "seven";
+        words[8] = "eight";
+        words[9] = "nine";
+        words[10] = "ten";
+
+        Log.v("NumbersActivity","Word at index0: " + words[0]);
+        Log.v("NumbersActivity","Word at index0: " + words[1]);
     }
+
 }
