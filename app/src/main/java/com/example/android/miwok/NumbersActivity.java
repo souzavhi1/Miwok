1c870d868fe2d36473d6683a5466f5f6fe32267e
Victor Hugo <souzavhi@gmail.com>
2017-10-05 12:27:54 -0300

    Loop´s

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -7,6 +7,13 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static com.example.android.miwok.R.id.rootView;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -15,21 +22,49 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String [] words = new String[11];
-        words[0] = "zero";
-        words[1] = "one";
-        words[2] = "two";
-        words[3] = "three";
-        words[4] = "four";
-        words[5] = "five";
-        words[6] = "six";
-        words[7] = "seven";
-        words[8] = "eight";
-        words[9] = "nine";
-        words[10] = "ten";
+        //String [] words = new String[10];
+        //words[0] = "zero";
 
-        Log.v("NumbersActivity","Word at index0: " + words[0]);
-        Log.v("NumbersActivity","Word at index0: " + words[1]);
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        //Log.v("NumbersActivity","Word at index0: " + words);
+        //Log.v("NumbersActivity","Word at index0: " + words.get(0));
+
+        // Acha uma Viewid e armazena em uma variavel
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+
+        // Cria um objeto e armazena na variavel e o tipo de dados é um TextView
+        //TextView wordView = new TextView(this);
+        //Modifica o texto a ser exibido da textview armazenada na variável.
+        //wordView.setText(words.get(0));
+        // Adiciona uma view filha em uma view pai
+        //rootView.addView(wordView);
+
+        // Loop While
+        //int count = 0;
+        //while (count < words.size()) {
+        //   TextView wordView = new TextView(this);
+        //    wordView.setText(words.get(count));
+        //    rootView.addView(wordView);
+        //   count += 1;
+
+        //Loop For
+        for (int count = 0;count < words.size(); count++) {
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(count));
+            rootView.addView(wordView);
+        }
+
     }
 
 }
